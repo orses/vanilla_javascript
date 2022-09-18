a62c53d90485a75897fb1ec67a1ffc7f95486628
Orses <orseton@gmail.com>
2022-09-18 19:33:23 +0200

    fixing some fetch_data files

--- fetch_pagination/src/assets/scripts/utils/fetch_data.js
+++ fetch_pagination/src/assets/scripts/utils/fetch_data.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 async function fetchJsonData(url) {
   let deliver = false;
   try {
@@ -16,22 +15,3 @@ async function fetchJsonData(url) {
 }
 
 export default fetchJsonData;
-=======
-async function fetchJsonData(url) {
-  let deliver = false;
-  try {
-    const response = await fetch(url);
-
-    if (response.ok) {
-      const responseData = await response.json();
-      deliver = responseData;
-    }
-  } catch (err) {
-    deliver = false;
-  }
-
-  return deliver;
-}
-
-export default fetchJsonData;
->>>>>>> 8214b560d7a6b0eca37d02a19be1f25d87d015b1
